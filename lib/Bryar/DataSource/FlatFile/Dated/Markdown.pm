b256b1e58b7a698d6e00117982a21e5ba1329de4
Richard Clamp <richardc@unixbeard.net>
2009-05-28 12:37:33 +0100

    doc basics

--- lib/Bryar/DataSource/FlatFile/Dated/Markdown.pm
+++ lib/Bryar/DataSource/FlatFile/Dated/Markdown.pm
@@ -23,3 +23,39 @@ sub make_document {
 }
 
 1;
+
+__END__
+=head1 NAME
+
+Bryar::DataSource::FlatFile::Dated::Markdown
+
+=head1 SYNOPSIS
+
+Add this to your C<bryar.conf>
+
+ source: Bryar::DataSource::FlatFile::Dated::Markdown
+ 
+And then you can format your entries using Markdown for the bodies, like so:
+
+ 2009-01-01Z00:00:01
+ Happy New Year
+ 
+ This blog is using [Markdown][] formatting as provided by [Text::Markdown][].
+ 
+ [Markdown]: http://daringfireball.net/projects/markdown/
+ [Text::Markdown]: http://search.cpan.org/dist/Text-Markdown/
+
+=head1 DESCRIPTION
+
+This is a simple datasource module that allows you to compose posts on your
+Bryar powered blog using Markdown.
+
+=head1 AUTHOR
+
+Richard Clamp <richardc@unixbeard.net>
+
+=head1 SEE ALSO
+
+L<Bryar> L<Text::Markdown>
+
+=cut
